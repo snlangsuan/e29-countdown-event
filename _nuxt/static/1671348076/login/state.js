3fbcc7bf3205bb20f9ba8cb4ff98041792413368
snlangsuan <snlangsuan@users.noreply.github.com>
2022-12-18 07:21:19 +0000

    deploy: a05d40db094447cf7d32eead8bfd018b08882fe2

---
+++ _nuxt/static/1671348076/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fe29-countdown-event\u002F_nuxt\u002Fstatic\u002F1671348076",layout:"default",error:a,state:{authUser:a},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002Fe29-countdown-event\u002F",assetsPath:"\u002Fe29-countdown-event\u002F_nuxt\u002F",cdnURL:a}}}}(null));
